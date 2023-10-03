202d4b451618611f5e6b92b40ede4c6f84f845a5
Heilos66 <2300423919@qq.com>
2023-10-03 20:54:36 +0800

    Site updated: 2023-10-03 20:54:36

--- js/search/algolia.js
+++ js/search/algolia.js
@@ -98,7 +98,7 @@ window.addEventListener('load', () => {
     showReset: false,
     showSubmit: false,
     searchOnEnterKeyPressOnly: true,
-    placeholder: GLOBAL_CONFIG.algolia.languages.input_placeholder,
+    placeholder: "输入关键词后，按下回车键即可搜索文章..."
     showLoadingIndicator: true
   })
 
