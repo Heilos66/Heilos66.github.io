23132a560b43a07432020d60c5b01386209eac7a
Heilos66 <2300423919@qq.com>
2023-10-04 22:10:14 +0800

    Site updated: 2023-10-04 22:10:14

--- js/search/algolia.js
+++ js/search/algolia.js
@@ -96,7 +96,7 @@ window.addEventListener('load', () => {
   const searchBox = instantsearch.widgets.searchBox({
     container: '#algolia-search-input',
     showReset: false,
-    showSubmit: true,
+    showSubmit: false,
     searchAsYouType: false, // 新增 可以实现回车或点击按钮搜索，不会每次输入都搜索
     // placeholder: GLOBAL_CONFIG.algolia.languages.input_placeholder
     // showLoadingIndicator: false
